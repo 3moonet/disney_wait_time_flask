7e6fd54d6a22d8d9f2e1a6f581f417ae231b48a5
3moonet <sanmoon429@gmail.com>
2021-03-10 01:01:13 +0900

    Initial Commit

--- run.py
+++ run.py
@@ -2,7 +2,8 @@ from main import app
 import multiprocessing
 from main.mksched import execute_schedule
 
-def run():
+
+if __name__ == "__main__":
     p1 = multiprocessing.Process(target=execute_schedule, args=('tdl',))
     p2 = multiprocessing.Process(target=execute_schedule, args=('tds',))
     p1.daemon = True
@@ -10,6 +11,4 @@ def run():
     p1.start()
     p2.start()
     app.run()
-
-if __name__ == "__main__":
-    run()
+    
